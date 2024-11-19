58ecfcde487d02ead4830d87e678f12959980c0d
Luke Stuckey <stuckeylu10@gmail.com>
2024-11-19 13:15:56 -0500

    lesson 87

---
+++ src/pages/[...catchAll].vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import AppErrorPage from '@/components/AppError/AppErrorPage.vue'
+
+useErrorStore().setError({ error: 'Page not found', customCode: 404 })
+</script>
+
+<template>
+  <div></div>
+</template>
